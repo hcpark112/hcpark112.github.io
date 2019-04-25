8e7afea4508511acdafb8e85ba030561dbf39bc4
hcpark112 <hpark112@my.bcit.ca>
2019-04-25 10:21:08 -0700

    menu changes

--- js/menus.js
+++ js/menus.js
@@ -1,19 +1,19 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the header
-var header = document.getElementById("header2");
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-    $("#header2").css("margin-top", "0");
-  } else {
-    $("#header2").css("margin-top", "56px");
-    header.classList.remove("sticky");
-  }
-}
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
+//
+// // Get the header
+// var header = document.getElementById("header2");
+//
+// // Get the offset position of the navbar
+// var sticky = header.offsetTop;
+//
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//     $("#header2").css("margin-top", "0");
+//   } else {
+//     $("#header2").css("margin-top", "56px");
+//     header.classList.remove("sticky");
+//   }
+// }
